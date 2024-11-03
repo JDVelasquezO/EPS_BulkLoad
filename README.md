8f1f202674e4a3c91dbbe274536365e0cf63c6a3
Daniel <jdvela158@gmail.com>
2024-11-03 14:52:08 -0600

    Create and insert temp_code_dependency

--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 ```npm start```
 
 # Endpoints
-| Método | Endpoint      | Puerto | Descripción                                                                                    |   |
-|--------|---------------|--------|------------------------------------------------------------------------------------------------|---|
-| GET    | /bulkLoadUser | 3001   | Carga los datos del archivo CSV a una tabla temporal en la base de datos.                      |   |
-| GET    | /cleanData    | 3001   | Limpia los datos eliminando registros duplicados, filas vacías y arregla correos electrónicos. |   |
-| GET    | /insertUser   | 3001   | Inserta los datos de la tabla temporal a la tabla usuario.                                     |   |
+| Método | Endpoint        | Puerto | Descripción                                                                                    |   |
+|--------|-----------------|--------|------------------------------------------------------------------------------------------------|---|
+| GET    | /bulkLoadUser   | 3001   | Carga los datos del archivo CSV a una tabla temporal en la base de datos.                      |   |
+| GET    | /cleanData      | 3001   | Limpia los datos eliminando registros duplicados, filas vacías y arregla correos electrónicos. |   |
+| GET    | /insertUser     | 3001   | Inserta los datos de la tabla temporal a la tabla usuario.                                     |   |
+| GET    | /deleteDataUser | 3001   | Elimina todo lo creado y modificado hasta ahora.                                               |   |
--- database/insertUser.js
+++ database/insertUser.js
@@ -1,6 +1,10 @@
 const queryInsertUser = `
     USE dev_deppa;
     
+    -- Reset old users
+    update usuario set usuario = '19160647' where usuario = '20160647';
+    update usuario set reg_tra = '19160647' where usuario = '19160647';
+    
     -- Password test
     insert into usuario (idUsuario, nombre, apellido, contrasenha, correo, usuario, reg_tra, fecha_nacimiento,
                          cui, genero, fecha_creacion, fecha_modificacion, idEstado, deleted_at)
@@ -15,6 +19,20 @@ const queryInsertUser = `
                     null
     from Temp;
     
+    DROP TABLE IF EXISTS temp_code_dependency;
+    CREATE TABLE IF NOT EXISTS temp_code_dependency (
+        csv int,
+        db int
+    );
+    
+    LOAD DATA
+    INFILE '/var/lib/mysql-files/view_code_dependency.csv' INTO TABLE temp_code_dependency
+    FIELDS TERMINATED BY ','
+    OPTIONALLY ENCLOSED BY '"'
+    LINES TERMINATED BY '\n'
+    IGNORE 1 LINES
+    (csv, db);
+    
     insert into unidad_academica_has_usuario (idUnidadAcademicaUsuario, descripcion,
         fecha_creacion, fecha_modificacion, idEstado, idUnidadAcademica, idUsuario, deleted_at)
     select null, DEPENDENCIA, CURDATE(),
@@ -22,7 +40,8 @@ const queryInsertUser = `
            ua.idUnidadAcademica, u.idUsuario, null
     from Temp
     inner join usuario u on u.reg_tra COLLATE utf8mb4_0900_ai_ci = Temp.REGISTRO
-    inner join unidad_academica ua on LOWER(ua.nombre) COLLATE utf8mb4_0900_ai_ci = LOWER(Temp.DEPENDENCIA);
+    inner join temp_code_dependency tcd on tcd.csv = Temp.CODIGO
+    inner join unidad_academica ua on ua.idUnidadAcademica = tcd.db;
 `;
 
 module.exports = queryInsertUser;
